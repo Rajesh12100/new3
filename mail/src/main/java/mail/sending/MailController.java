1750b1b578c9134afc20c6303bd56c0bc119cf39
91837 <91837@LAPTOP-6T56M1GP.bbrouter>
2023-04-17 14:58:39 +0530

    second commit

---
+++ mail/src/main/java/mail/sending/MailController.java
@@ -0,0 +1,14 @@
+package mail.sending;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MailController {
+
+	@RequestMapping("/welcome")
+	public String welcome() {
+		
+		return "hello";
+	}
+}
---
+++ mail/src/main/java/mail/sending/service/EmailService.java
@@ -0,0 +1,8 @@
+package mail.sending.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+	
+}
